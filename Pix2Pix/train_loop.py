5375f439f9ce0120ed43b36976a3e90ee0b8b2ce
Ameli <rain0210303@gmail.com>
2023-01-20 22:16:55 +0800

    load

--- Pix2Pix/train_loop.py
+++ Pix2Pix/train_loop.py
@@ -24,3 +24,14 @@ if __name__ == "__main__":
     val_dataset = pix2pixDataset(config.val_dir)
     val_loader = DataLoader(val_dataset, config.batch_size,
                             num_workers=config.num_workers)
+
+    if config.load_model:
+        checkpoint = torch.load(config.model_checkpoint)
+        disc.load_state_dict(checkpoint["disc_model"])
+        disc_opt.load_state_dict(checkpoint["disc_opt"])
+        for param_group in disc_opt.param_groups:
+            param_group["lr"] = config.lr
+        gen.load_state_dict(checkpoint["gen_model"])
+        gen_opt.load_state_dict(checkpoint["gen_opt"])
+        for param_group in gen_opt.param_groups:
+            param_group["lr"] = config.lr
