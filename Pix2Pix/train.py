4588bd267bc86614d2e8a91e2a8fe6f9a1ce4237
Ameli <rain0210303@gmail.com>
2023-01-21 01:18:51 +0800

    *

--- Pix2Pix/train.py
+++ Pix2Pix/train.py
@@ -1,101 +1,106 @@
 import torch
-from utils import save_checkpoint, load_checkpoint, save_some_examples
 import torch.nn as nn
-import torch.optim as optim
-import config
-from dataset import pix2pixDataset
-from Generator import Generator
-from Discriminator import Discriminator
 from torch.utils.data import DataLoader
-from tqdm import tqdm
-from torchvision.utils import save_image
+from torchvision.utils import make_grid, save_image
+import logging
+import tqdm
+import config
+from Discriminator import Discriminator
+from Generator import Generator
+from dataset import pix2pixDataset
 
 torch.backends.cudnn.benchmark = True
 
-
-def train_fn(
-    disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, g_scaler, d_scaler,
-):
-    loop = tqdm(loader, leave=True)
-
-    for idx, (x, y) in enumerate(loop):
-        x = x.to(config.DEVICE)
-        y = y.to(config.DEVICE)
-
-        # Train Discriminator
-        with torch.cuda.amp.autocast():
-            y_fake = gen(x)
-            D_real = disc(x, y)
-            D_real_loss = bce(D_real, torch.ones_like(D_real))
-            D_fake = disc(x, y_fake.detach())
-            D_fake_loss = bce(D_fake, torch.zeros_like(D_fake))
-            D_loss = (D_real_loss + D_fake_loss) / 2
-
-        disc.zero_grad()
-        d_scaler.scale(D_loss).backward()
-        d_scaler.step(opt_disc)
-        d_scaler.update()
-
-        # Train generator
-        with torch.cuda.amp.autocast():
-            D_fake = disc(x, y_fake)
-            G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
-            L1 = l1_loss(y_fake, y) * config.L1_LAMBDA
-            G_loss = G_fake_loss + L1
-
-        opt_gen.zero_grad()
-        g_scaler.scale(G_loss).backward()
-        g_scaler.step(opt_gen)
-        g_scaler.update()
-
-        if idx % 10 == 0:
-            loop.set_postfix(
-                D_real=torch.sigmoid(D_real).mean().item(),
-                D_fake=torch.sigmoid(D_fake).mean().item(),
-            )
-
-
-def main():
-    disc = Discriminator(in_channels=3).to(config.DEVICE)
-    gen = Generator(in_channels=3, features=64).to(config.DEVICE)
-    opt_disc = optim.Adam(
-        disc.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999),)
-    opt_gen = optim.Adam(
-        gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
-    BCE = nn.BCEWithLogitsLoss()
-    L1_LOSS = nn.L1Loss()
-
-    if config.LOAD_MODEL:
-        load_checkpoint(
-            config.CHECKPOINT_GEN, gen, opt_gen, config.LEARNING_RATE,
-        )
-        load_checkpoint(
-            config.CHECKPOINT_DISC, disc, opt_disc, config.LEARNING_RATE,
-        )
-
-    train_dataset = pix2pixDataset(root_dir=config.TRAIN_DIR)
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=config.BATCH_SIZE,
-        shuffle=True,
-        num_workers=config.NUM_WORKERS,
-    )
-    g_scaler = torch.cuda.amp.GradScaler()
-    d_scaler = torch.cuda.amp.GradScaler()
-    val_dataset = pix2pixDataset(root_dir=config.VAL_DIR)
-    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
-
-    for epoch in range(config.NUM_EPOCHS):
-        train_fn(
-            disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,
-        )
-
-        if config.SAVE_MODEL and epoch % 5 == 0:
-            save_checkpoint(gen, opt_gen, filename=config.CHECKPOINT_GEN)
-            save_checkpoint(disc, opt_disc, filename=config.CHECKPOINT_DISC)
-
-        save_some_examples(gen, val_loader, epoch, folder="evaluation")
-
-
 if __name__ == "__main__":
-    main()
+    # Basic structure
+    disc = Discriminator()
+    disc_opt = torch.optim.Adam(
+        disc.parameters(), lr=config.lr, betas=(0.5, 0.999))
+    gen = Generator()
+    gen_opt = torch.optim.Adam(
+        gen.parameters(), lr=config.lr, betas=(0.5, 0.999))
+    bceloss_func = nn.BCEWithLogitsLoss()
+    l1loss_func = nn.L1Loss()
+    train_dataset = pix2pixDataset(config.train_dir)
+    train_loader = DataLoader(
+        train_dataset, config.batch_size, shuffle=True, num_workers=config.num_workers)
+    val_dataset = pix2pixDataset(config.val_dir)
+    val_loader = DataLoader(val_dataset, config.batch_size,
+                            num_workers=config.num_workers)
+    # Logging file
+    logging.basicConfig(filename=config.logfile_name,
+                        format="%(asctime)s %(levelname)s %(message)s")
+    logfile = logging.getLogger()
+
+    # Loading previous model
+    if config.load_model:
+        checkpoint = torch.load(config.model_checkpoint)
+        disc.load_state_dict(checkpoint["disc_model"])
+        disc_opt.load_state_dict(checkpoint["disc_opt"])
+        for param_group in disc_opt.param_groups:
+            param_group["lr"] = config.lr
+        gen.load_state_dict(checkpoint["gen_model"])
+        gen_opt.load_state_dict(checkpoint["gen_opt"])
+        for param_group in gen_opt.param_groups:
+            param_group["lr"] = config.lr
+
+    # Pre-process for recording sample generated image
+    if not config.val_imageset_dir.exists():
+        config.val_imageset_dir.mkdir()
+        for index, (colored_image, uncolored_image) in enumerate(val_loader):
+            original_image = make_grid(colored_image)
+            save_image(original_image, config.val_imageset_dir /
+                       f"set{index}.png")
+
+    if not config.val_sample_dir.exists():
+        config.val_sample_dir.mkdir()
+
+    for epoch in tqdm.trange(config.epochs):
+        # Training part
+        # Check models' are in the training mode
+
+        if not (disc.training and gen.training):
+            logfile.error("Models stuck in the eval mode")
+        for index, (colored_image, uncolored_image) in enumerate(train_loader):
+            # Discriminator part
+            gen_image = gen(uncolored_image)
+            disc_real = disc(uncolored_image, colored_image)
+            disc_fake = disc(uncolored_image, gen_image)
+            disc_loss = (bceloss_func(disc_real, torch.ones_like(
+                disc_real)) + bceloss_func(disc_fake, torch.zeros_like(disc_fake))) / 2
+            disc.zero_grad()
+            disc_loss.backward(retain_graph=True)
+            disc_opt.step()
+
+            # Generator part
+            disc_real = disc(uncolored_image, colored_image)
+            disc_fake = disc(uncolored_image, gen_image)
+            bceloss = bceloss_func(disc_real, torch.ones_like(
+                disc_real)) + bceloss_func(disc_fake, torch.zeros_like(disc_fake))
+            gen_loss = bceloss + \
+                config.l1_lambda * l1loss_func(gen_image, colored_image)
+            gen.zero_grad()
+            gen_loss.backward()
+            gen_opt.step()
+
+            if index % 100 == 0:
+                print(
+                    f"epoch: {epoch}/{config.epochs} batch: {index}/{len(train_loader)}")
+                print(
+                    f"generator loss: {gen_loss} discriminator loss: {disc_loss}")
+
+        # Validation part
+        for index, (colored_image, uncolored_image) in enumerate(val_loader):
+            with torch.inference_mode():
+                # Record sample of generated image
+                gen_image = gen(uncolored_image)
+                gen_imageset = make_grid(gen_image)
+                save_image(gen_image, f"epoch{epoch}_image{index}.png")
+                # Checkpoint of models
+                model_state = {
+                    "disc_model": disc.state_dict(),
+                    "disc_opt": disc_opt.state_dict(),
+                    "gen_model": gen.state_dict(),
+                    "gen_opt": gen_opt.state_dict(),
+                }
+                torch.save(model_state, config.model_checkpoint)
