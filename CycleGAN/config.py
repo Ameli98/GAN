2fc761048741ec4dd1c8c45608bf3e7cae47bc84
Ameli <rain0210303@gmail.com>
2023-02-01 22:46:11 +0800

    *

--- CycleGAN/config.py
+++ CycleGAN/config.py
@@ -3,7 +3,8 @@ import albumentations as alb
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 lr = 2e-4
-lamb = 10
+lambda_cycle = 10
+lambda_identity = 0
 batch_size = 1
 num_workers = 4
 epochs = 10
