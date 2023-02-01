4382d68c237ec0d1785db53faee4a1078c07e80a
Ameli <rain0210303@gmail.com>
2023-02-01 17:06:38 +0800

    discriminator model completed

---
+++ CycleGAN/discriminator.py
@@ -0,0 +1,30 @@
+from torch import nn
+
+
+class discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(3, 64, 4, padding=1, padding_mode="reflect"),
+            nn.LeakyReLU(0.2),
+            c4s2(64, 128),
+            c4s2(128, 256),
+            c4s2(256, 512),
+        )
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+class c4s2(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 4,
+                      padding=1, padding_mode="reflect"),
+            nn.InstanceNorm2d(out_channel),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.layer(x)
