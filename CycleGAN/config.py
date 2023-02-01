1d19d2909bc0a517f5ccca446b831194678fe098
Ameli <rain0210303@gmail.com>
2023-02-01 22:45:03 +0800

    some hyperparameters setting

---
+++ CycleGAN/config.py
@@ -0,0 +1,19 @@
+import torch
+import albumentations as alb
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+lr = 2e-4
+lamb = 10
+batch_size = 1
+num_workers = 4
+epochs = 10
+
+transform = alb.Compose(
+    [
+        alb.Resize(width=256, height=256),
+        alb.HorizontalFlip(0.5),
+        alb.Normalize(mean=[0.5, 0.5, 0.5], std=[
+                      0.5, 0.5, 0.5], max_pixel_value=255),
+        alb.pytorch.ToTensorV2(),
+    ]
+)
