6df072a9b9deed7cda395da946c1352a7211b307
Ameli98 <rain0210303@gmail.com>
2023-02-16 19:23:53 +0800

    discriminator init

---
+++ ProGAN/discriminator.py
@@ -0,0 +1,66 @@
+import torch
+from torch import nn, Tensor
+from generator import DoubleConv, Eqlr_Conv, interpolation
+
+
+def minibatch(input:Tensor) -> Tensor:
+    constant = torch.std(input, dim=0).mean().repeat(input.shape[0], 1, input.shape[2], input.shape[3])
+    output =  torch.cat((input, constant), dim=1)
+    return output
+
+
+class Discriminator(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # progresivce layer
+        self.layerlist = nn.ModuleList([])
+        for i in range(3):
+            self.layerlist.append(DoubleConv(2**(i+6), 2**(i+7), pixnorm=False, inv=True))
+        for i in range(3):
+            self.layerlist.append(DoubleConv(512, 512, pixnorm=False, inv=True))
+
+        # RGB layer
+        self.RGBlayer = nn.ModuleList([])
+        for i in range(3):
+            self.RGBlayer.append(Eqlr_Conv(3, 2**(6+i), 1, 0))
+        for i in range(3):
+            self.RGBlayer.append(Eqlr_Conv(3, 512, 1, 0))
+        
+        # final layer
+        self.finallayer = nn.Sequential(
+            Eqlr_Conv(513, 512),
+            nn.LeakyReLU(0.2),
+            Eqlr_Conv(512, 512, 4, 0)
+        )
+    
+    # Binding final layer with minibatch
+    def ending(self, input:Tensor) -> int:
+        output = minibatch(input)
+        output = self.finallayer(output)
+        output = torch.squeeze(output)
+        linear = nn.Linear(512, 1)
+        return linear(output)
+
+
+    def forward(self, input:Tensor, phase:int, alpha:float):
+        if phase == 0:
+            return self.ending(input)
+        pool = nn.AvgPool2d(2)
+        downsampled = pool(input)
+        downsampled_nRGB =  self.RGBlayer[7-phase](downsampled)
+        conved_nRGB = self.RGBlayer[6-phase](input)
+        conved_nRGB = pool(self.layerlist[6-phase](conved_nRGB))
+
+        output = interpolation(alpha, downsampled_nRGB, conved_nRGB)
+        for layer in self.layerlist[7-phase:]:
+            output = layer(output)
+            output = pool(output)
+        return self.ending(output)
+
+
+if __name__ == "__main__":
+    d = Discriminator()
+    x = torch.randn(4, 3, 256, 256)
+    y = d(x, 6, 1)
+    print(y.shape)
--- ProGAN/generator.py
+++ ProGAN/generator.py
@@ -20,55 +20,51 @@ class Generator(nn.Module):
         )
         for i in range(3):
             self.layerlist.append(DoubleConv(512, 512))
-        for i in range(5):
+        for i in range(3):
             self.layerlist.append(DoubleConv(2**(9-i), 2**(8-i)))
 
         # RGB layer construction
-        self.RGBlayer = nn.ModuleList([])
+        self.RGBlayer = nn.ModuleList([Eqlr_Conv(512, 3, 1, 0)])
         for i in range(4):
-            self.RGBlayer.append(Eqlr_Conv(512, 3, 1))
-        for i in range(4):
-            self.RGBlayer.append(Eqlr_Conv(2**(8-i), 3, 1))
+            self.RGBlayer.append(Eqlr_Conv(512, 3, 1, 0))
+        for i in range(3):
+            self.RGBlayer.append(Eqlr_Conv(2**(8-i), 3, 1, 0))
 
-    def interpolation(self, alpha:float, upsampled:torch.Tensor, conved:torch.Tensor):
-        return (alpha * conved) + ((1-alpha) * upsampled)
+        # Final Tanh
+        self.final = nn.Tanh()
 
-    def forward(self, x, phase, alpha):
+    def forward(self, x, phase:int, alpha:float):
         conved = self.layerlist[0](x)
         if phase == 0:
-            return nn.Tanh(conved)
-        if alpha == 1:
-            for layer in self.layerlist[1:phase-1]:
-                conved = layer(conved)
-            return nn.Tanh(conved)
+            return self.final(conved)
         for step in range(phase):
             upsampled = nn.functional.interpolate(conved, scale_factor=2)
             conved = self.layerlist[step+1](upsampled)
-        upsampled_RGB, conved_RGB = self.RGBlayer[phase-1], self.RGBlayer[phase]
-        return nn.Tanh(self.interpolation(alpha, upsampled_RGB, conved_RGB))
+        upsampled_RGB, conved_RGB = self.RGBlayer[phase](upsampled), self.RGBlayer[phase+1](conved)
+        return self.final(interpolation(alpha, upsampled_RGB, conved_RGB))
 
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channel, out_channel, pixnorm=True) -> None:
+    def __init__(self, in_channel, out_channel, padding=1, pixnorm=True, inv:bool=False) -> None:
         super().__init__()
+        mid_channel = in_channel if inv else out_channel
         self.layerlist = nn.ModuleList([
-            Eqlr_Conv(in_channel, out_channel, 3),
-            Eqlr_Conv(in_channel, out_channel, 3)
+            Eqlr_Conv(in_channel, mid_channel, 3, padding),
+            Eqlr_Conv(mid_channel, out_channel, 3, padding)
         ])
-        self.pixnorm = pixnorm
-        
+        self.transform = PixNorm() if pixnorm else nn.Identity()
 
     def forward(self, x):
         for layer in self.layerlist:
             x = layer(x)
-            x =  PixNorm(x) if self.pixnorm else x
+            x =  self.transform(x)
         return x
 
 class Eqlr_Conv(nn.Module):
-    def __init__(self, in_channel, out_channel, kernal_size=3) -> None:
+    def __init__(self, in_channel, out_channel, kernal_size=3, padding=1) -> None:
         super().__init__()
         self.scaler = (2/(in_channel * kernal_size * kernal_size)) ** 0.5
-        self.conv = nn.Conv2d(in_channel, out_channel, kernal_size)
+        self.conv = nn.Conv2d(in_channel, out_channel, kernal_size, padding=padding)
         nn.init.normal_(self.conv.weight)
         nn.init.zeros_(self.conv.bias)
         self.activation = nn.LeakyReLU(0.2)
@@ -83,4 +79,13 @@ class PixNorm(nn.Module):
         super().__init__()
     
     def forward(self, input):
-        return torch.sqrt(torch.mean(input ** 2, dim=1, keepdim=True))
+        return input / torch.sqrt(torch.mean(input ** 2, dim=1, keepdim=True) + 1e-8)
+
+def interpolation(alpha:float, upsampled:torch.Tensor, conved:torch.Tensor):
+        return (alpha * conved) + ((1-alpha) * upsampled)
+
+if __name__ == "__main__":
+    g = Generator()
+    x = torch.randn(4, 512, 1, 1)
+    y = g(x, 4, 0.5)
+    print(y.shape)
